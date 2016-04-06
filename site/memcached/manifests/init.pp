73229cf8e6cb995bf51e3a7b04865bca81016b53
Scott D. Miller <sxm77@psu.edu>
2016-04-06 11:31:36 -0400

    adding dirs/files for 11.2

---
+++ site/memcached/manifests/init.pp
@@ -0,0 +1,20 @@
+class memcached{
+  package{'memcached':
+   ensure => present,
+   }
+   
+   file {'/etc/sysconfig/memcached':
+     ensured => file,
+     owner => 'root',
+     group => 'root',
+     mode => '0644',
+     source => 'puppet:///modules/memcached/memcached',
+     required => Package['memcached'],
+     }
+     
+    service {'memcached':
+      ensure => running,
+      enable => true,
+      subscribe => File['/etc/sysconfig/memcached'],
+	}
+}
